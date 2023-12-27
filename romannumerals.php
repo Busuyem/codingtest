9bb63b0799c9efc44b866325d37a5dbe182e7c0b
busuyem <owoyemibusuyi@gmail.com>
2023-12-27 18:47:39 +0100

    add questions

--- romannumerals.php
+++ romannumerals.php
@@ -1,5 +1,15 @@
 <?php
 
+//Question
+/*
+Have the function StringChallenge(str) read str which will be a string of roman numerals in decreasing order.
+The numerals being used are: I for 1, V for 5, X for 10, L for 50, C for 100, D for 500 and M for 1000.
+Your program should return the same number given by str using a smaller set of roman numerals. 
+For example: if str is "LLLXXXVVVV" this is 200, so your program should return CC because this is the shortest way to write 200 using the roman numeral system given above. 
+If a string is given in its shortest form, just return that same string.
+*/
+
+
 function shortestRomanRepresentation($str) {
     $romanNumerals = [
         'I' => 1,
@@ -52,4 +62,59 @@ $result = shortestRomanRepresentation($inputStr);
 
 echo "Shortest representation for $inputStr is: $result\n";
 
+
+
+/**
+ * __define-ocg__
+ * This function takes a string of Roman numerals in decreasing order
+ * and returns the same number using a smaller set of Roman numerals.
+ *
+ * @param string $str The input string of Roman numerals.
+ * @return string The Roman numeral representation with a smaller set of numerals.
+ */
+/*
+function StringChallenge($str) {
+    $romanNumerals = [
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000,
+    ];
+
+    $varOcg = ''; // Variable with the requested name
+
+    $result = '';
+    $prevNumeral = '';
+
+    // Iterate through the input string
+    for ($i = 0; $i < strlen($str); $i++) {
+        $currentNumeral = $str[$i];
+
+        // If the current numeral is greater than the previous one,
+        // subtract twice the previous value (as it was added in the previous iteration)
+        if ($romanNumerals[$currentNumeral] > $romanNumerals[$prevNumeral]) {
+            $result = substr($result, 0, -1); // Remove the last character
+            $result .= $currentNumeral;
+            $result .= $prevNumeral;
+        } else {
+            $result .= $currentNumeral;
+        }
+
+        $prevNumeral = $currentNumeral;
+    }
+
+    return $result;
+}
+
+// Example usage
+$inputStr = 'LLLXXXVVVV';
+$result = StringChallenge($inputStr);
+
+echo "Result for $inputStr is: $result\n";
+
 ?>
+
+
